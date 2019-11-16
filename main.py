68ae4352b7b662175804622f5adefe2d5ce30931
Ville-Matti Tanninen <ville-matti.tanninen@outlook.com>
2019-11-16 17:42:43 +0200

    Moved the site from /path/v1.1 to site root.

--- main.py
+++ main.py
@@ -5,23 +5,14 @@ from flask import Flask, request, render_template
 app = Flask(__name__)
 
 # http://0.0.0.0:80/api/v1.1?deg=13.0
-@app.route('/api/<path>', methods=["post", "get"])
-def main(path):
+@app.route('/')
+def main():
     """Handle main path."""
-    if path == "v1.1":
-        deg = request.args.get('deg', default=0.0, type=float)
-        print(deg)
-        #return str(deg)
-        return render_template("index.html")
-    return "Try /api/v1.1 instead."
+    return render_template("index.html")
 
 @app.route('/3D/')
 def threedee():
     return render_template("3D.html")
-
-@app.route("/")
-def hello():
-    return "Hello Docker World!"
 
 if __name__ == '__main__':
      app.run(host='0.0.0.0', port=5000)

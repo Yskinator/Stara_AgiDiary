85c12694d7d90187a403fac831d6da6a1231348b
MagusDei <jami.valorinta@helsinki.fi>
2019-11-16 22:14:41 +0200

    Moved script to custom.js from index.html, implemented image adding for new posts

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,6 +1,6 @@
 var leftside = true;
 
-function addPost(){
+function addPost(counter){
     //Create parent div on the left or right side of the timeline
     var newDiv = document.createElement("div");
     if(leftside){
@@ -18,10 +18,71 @@ function addPost(){
     //Create post text
     var txt = document.createElement("P");
     txt.innerHTML = "Lorem ipsum dolor sit amet, quo ei simul congue exerci";
+    var newDiv3 = document.createElement("div");
+    newDiv3.innerHTML = '<form method="post" enctype="multipart/form-data"> \
+          <div> \
+            <label for="image_upload_'+counter+'">Lisää kuva...</label> \
+            <input type="file" id="image_upload_'+counter+'" name="image_upload" accept="image/*" capture="camera"> \
+          </div> \
+          <div class="preview"> \
+            <p>Ei kuvaa lisätty.</p> \
+          </div> \
+          <div> \
+            <button>Lisää</button>\
+          </div> \
+        </form>';
 
     newDiv2.appendChild(h2);
     newDiv2.appendChild(txt);
+    newDiv2.appendChild(newDiv3.firstChild);
     newDiv.appendChild(newDiv2);
     mytimeline.appendChild(newDiv);
+    updatePosts();
 }
 
+function updatePosts() {
+    var posts = document.querySelectorAll('.content');
+
+    posts.forEach( function (post) {
+
+      var input = post.querySelector('input');
+      if (input === null) {
+        return;
+      }
+      input.preview = post.querySelector('.preview');
+      
+      console.log(input.preview);
+
+      input.style.opacity = 0;
+
+      input.addEventListener('change', function (e) {
+          e.preventDefault();
+          
+          while (preview.firstChild) {
+              preview.removeChild(preview.firstChild);
+          }
+          
+          var curFiles = input.files;
+          
+          if (curFiles.length === 0) {
+              var para = document.createElement('p');
+              para.textContent = "Ei tiedostoa valittu.";
+              preview.appendChild(para);
+          } else {
+              if (validFileType(curFiles[0])) {
+                  var image = document.createElement('img');
+                  image.src = window.URL.createObjectURL(curFiles[0]);
+                  preview.appendChild(image);
+              }
+          }
+      });
+      
+      function validFileType(file) {
+          return true;
+      }
+    });
+}
+
+window.addEventListener('DOMContentLoaded', (event) => {
+    updatePosts();
+});
